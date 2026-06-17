fa621f683be47bcd2ad4106fcae518bcaa01c7e0
0xNinx <kidssafarischool@gmail.com>
2026-06-17 03:52:07 +0100

    feat: integrate websockets into the main application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { WebsocketsModule } from './websockets/websockets.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, WebsocketsModule],
   controllers: [AppController],
   providers: [AppService],
 })
